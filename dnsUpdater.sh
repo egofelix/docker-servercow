575f2cea04a2e23e846337a95304b8db1b24d564
felix <git@egofelix.de>
2021-01-24 18:10:47 +0100

    Added support to specify hostnames directly

--- dnsUpdater.sh
+++ dnsUpdater.sh
@@ -7,13 +7,6 @@ set -uo pipefail
 source "${BASH_SOURCE%/*}/functions.sh"
 
 # Check Variables
-if [[ -z ${HOSTNAME:-} ]]; then
-  HOSTNAME=$(cat /proc/sys/kernel/hostname)
-fi;
-if [[ -z ${HOSTNAME} ]]; then
-  echo "Please provide HOSTNAME environment variable.";
-  exit 1;
-fi;
 if [[ -z ${USERNAME:-} ]]; then
   echo "Please provide USERNAME environment variable.";
   exit 1;
@@ -22,10 +15,31 @@ if [[ -z ${PASSWORD:-} ]]; then
   echo "Please provide PASSWORD environment variable.";
   exit 1;
 fi;
-if [[ -z ${DOMAINS:-} ]]; then
-  echo "Please provide DOMAINS environment variable.";
-  exit 1;
+if [[ -z ${HOSTNAMES:-} ]]; then
+  if [[ -z ${HOSTNAME:-} ]]; then
+    HOSTNAME=$(cat /proc/sys/kernel/hostname)
+  fi;
+  if [[ -z ${HOSTNAME} ]]; then
+    echo "Please provide HOSTNAME environment variable.";
+    exit 1;
+  fi;
+  if [[ -z ${DOMAINS:-} ]]; then
+    echo "Please provide DOMAINS environment variable.";
+    exit 1;
+  fi;
+
+  # Trim Hostname (if it contains any .)
+  HOSTNAME=$(echo "${HOSTNAME}" | awk -F'.' '{print $1}')
+  HOSTNAMES=()
+
+  for DOMAIN in ${DOMAINS}
+  do
+    HOSTNAMES+=(${HOSTNAME}.${DOMAIN})
+  done;
+
+  HOSTNAMES="${HOSTNAMES[@]}"
 fi;
+
 if [[ -z ${TTL:-} ]]; then
   TTL="60";
 fi;
@@ -39,26 +53,39 @@ if [[ -z ${UPDATE_IPV6:-} ]]; then
   UPDATE_IPV6="true";
 fi;
 
-# Trim Hostname (if it contains any .)
-HOSTNAME=$(echo "${HOSTNAME}" | awk -F'.' '{print $1}')
+logLine "Hostnames: ${HOSTNAMES}";
 
 # Main Loop
 while true; do
+
   # Get current IPs
   IPV4=$(curl --silent ifconfig.me)
-  IPV6=$(LANG=C ip addr list | grep 'inet6' | grep 'global' | awk '{print $2}' | awk -F'/' '{print $1}')
+  IPV6=$(LANG=C ip addr list | grep 'inet6' | grep 'global' | awk '{print $2}' | awk -F'/' '{print $1}' | grep -v '2001:')
 
-  # Loop over domains
-  for DOMAIN in ${DOMAINS}
+  # Loop over hostnames
+  for HOST in ${HOSTNAMES}
   do
+    logLine "${HOST}";
+    POINTCOUNT=$(echo ${HOST} | awk -F'.' '{print NF-1}');
+    if [[ ${POINTCOUNT} == 1 ]]; then
+      SUBDOMAIN="";
+      DOMAIN="${HOST}";
+    elif [[ ${POINTCOUNT} == 2 ]]; then
+      SUBDOMAIN=$(echo ${HOST} | awk -F'.' '{print $1}');
+      DOMAIN=$(echo ${HOST} | cut -d'.' -f2-);
+    else
+      logLine "Unsupported Hostname: ${HOST}";
+      continue;
+    fi;
+
     if isTrue ${UPDATE_IPV4} && [[ ! -z "${IPV4}" ]]; then
-      updateRecord "A" ${HOSTNAME} ${DOMAIN} ${IPV4};
+      updateRecord "A" "${SUBDOMAIN}" "${DOMAIN}" "${IPV4}";
     fi;
     if isTrue ${UPDATE_IPV6} && [[ ! -z "${IPV6}" ]]; then
-      updateRecord "AAAA" ${HOSTNAME} ${DOMAIN} ${IPV6};
+      updateRecord "AAAA" "${SUBDOMAIN}" "${DOMAIN}" "${IPV6}";
     fi;
   done;
 
-  # Sleep 30 seconds
-  sleep 30;
+  # Sleep TTL seconds
+  sleep ${TTL};
 done;
--- functions.sh
+++ functions.sh
@@ -14,35 +14,42 @@ function logLine {
 
 function updateRecord {
   local TARGET_RECORD="$1"
-  local TARGET_HOSTNAME="$2"
+  local TARGET_SUBDOMAIN="$2"
   local TARGET_DOMAIN="$3"
   local TARGET_IP="$4"
   local EXPECTEDRESULT="{\"message\":\"ok\"}"
 
+#  echo "Update Record: \"${TARGET_RECORD}\", \"${TARGET_SUBDOMAIN}\", \"${TARGET_DOMAIN}\", \"${TARGET_IP}\"...";
+
+  if [[ -z "${TARGET_SUBDOMAIN}" ]]; then
+    local FULL_DOMAIN="${TARGET_DOMAIN}";
+  else
+    local FULL_DOMAIN="${TARGET_SUBDOMAIN}.${TARGET_DOMAIN}";
+  fi;
+
   if [[ -z "${TARGET_IP}" ]]; then
-    echo "Cannot update ${TARGET_RECORD}-Record of ${TARGET_HOSTNAME}.${TARGET_DOMAIN} without a ip ${TARGET_IP}";
+    echo "Missing Parameter #4: TARGET_IP";
     return 1;
   fi;
 
   # Check if current ip matchs
-  local CURRENT_IP=$(dig ${TARGET_HOSTNAME}.${TARGET_DOMAIN} ${TARGET_RECORD} +short)
+  local CURRENT_IP=$(dig ${FULL_DOMAIN} ${TARGET_RECORD} +short)
   if [[ "${CURRENT_IP}" = "${TARGET_IP}" ]]; then
-    logLine "${TARGET_RECORD}-Record of \"${HOSTNAME}.${DOMAIN}\" is up to date.";
+    logLine "${TARGET_RECORD}-Record of \"${FULL_DOMAIN}\" is up to date.";
     return 0;
   else
-    logLine "Updating ${TARGET_RECORD}-Record of ${TARGET_HOSTNAME}.${TARGET_DOMAIN} with ${TARGET_IP}...";
+    logLine "Updating ${TARGET_RECORD}-Record of \"${FULL_DOMAIN}\" with ${TARGET_IP}...";
     local RESULT=$(curl --silent -X POST "https://api.servercow.de/dns/v1/domains/${TARGET_DOMAIN}" \
                            -H "X-Auth-Username: ${USERNAME}" \
                            -H "X-Auth-Password: ${PASSWORD}" \
                            -H "Content-Type: application/json" \
-                           --data "{\"type\":\"${TARGET_RECORD}\",\"name\":\"${TARGET_HOSTNAME}\",\"content\":\"${TARGET_IP}\",\"ttl\":${TTL}}")
+                           --data "{\"type\":\"${TARGET_RECORD}\",\"name\":\"${TARGET_SUBDOMAIN}\",\"content\":\"${TARGET_IP}\",\"ttl\":${TTL}}")
     if [[ "${RESULT}" = "${EXPECTEDRESULT}" ]]; then
       logLine "Updated";
       return 0;
     else
-      echo "Failed to update ${TARGET_RECORD}-Record of host \"${TARGET_HOSTNAME}.${TARGET_DOMAIN}\" with \"${TARGET_IP}\": ${RESULT}";
+      echo "Failed to update ${TARGET_RECORD}-Record of host \"${FULL_DOMAIN}\" with \"${TARGET_IP}\": ${RESULT}";
       return 1;
     fi;
   fi;
 }
-
